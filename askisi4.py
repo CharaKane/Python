48b12f58e7921bd341346a304f0372cc92f8ec4d
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-23 17:49:01 +0200

    python

---
+++ askisi4.py
@@ -0,0 +1,10 @@
+
+a= int( input ("Dose ton proto arithmo: "))
+b =int( input ("Dose ton deutero arithmo: "))
+c =int( input ("Dose ton trito arithmo: "))
+
+d=a+b+c
+mo=d/3
+
+print ("To arithrisma einai:", d)
+print ("O mesos oros einai :", mo)
