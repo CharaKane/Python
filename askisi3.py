82d1c0c65381cd6837dfacd1f707202cd5ac25be
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-23 17:45:02 +0200

    python

---
+++ askisi3.py
@@ -0,0 +1,10 @@
+
+a= int( input ("Dose ton proto arithmo: "))
+b =int( input ("Dose ton deutero arithmo: "))
+
+c=a
+a=b
+b=c
+
+print ("o protos arithmos einai:", a)
+print ("o deuteros arithmos einai:", b)
