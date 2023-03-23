923499fc27399a21b1086af4e23d2f2e9627b1d9
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-23 18:18:31 +0200

    python

---
+++ askisi6.py
@@ -0,0 +1,12 @@
+
+A= int( input ("Dose enan 4traphifio arithmo: "))
+
+xil=A//1000
+ekat=(A-xil*1000)//100
+dek=(A-xil*1000-ekat*100)//10
+mon=(A-xil*1000-ekat*100-dek*10)//1
+
+sum=xil+ekat+dek+mon
+
+
+print ("To athrisma ton phifion einai:", sum)
