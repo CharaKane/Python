c23a5c4e7eeba530e2605b1542e2d62ab78e0cd6
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 18:01:44 +0300

    megaliteros arithmos

---
+++ askisi12.py
@@ -0,0 +1,8 @@
+a= int (input ("dose ton proto arthmo: "))
+b= int (input ("dose ton deutero arithmo : "))
+
+if a>b:
+    print("O megaliteros arithmos einai o : " , a)
+
+else:
+    print("O megaliteros arithmos einai o : " , b)
