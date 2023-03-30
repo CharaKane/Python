d42a1b28733667d536a40596215edede82f93735
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 18:49:45 +0300

    if

---
+++ askisi13.py
@@ -0,0 +1,14 @@
+Sum1= int (input ("dose to Sum1: "))
+Sum2= int (input ("dose to Sum2: "))
+x= int (input ("dose to x: "))
+
+
+if x>=0:
+    Sum1=Sum1+x
+    Sum2=Sum2+x
+    print("To Sum1 einai: " , Sum1, "kai to Sum2 einai:" , Sum2 )
+
+else:
+    Sum1=Sum1-x
+    Sum2=Sum2-x
+    print("To Sum1 einai: " , Sum1, "kai to Sum2 einai:" , Sum2 )
