1639e2cdbbd85007fa94eaf8e77a05e7bdbe4cf7
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 19:00:01 +0300

    diakrinousa

--- askisi10.py
+++ askisi10.py
@@ -6,11 +6,11 @@ c= int (input ("dose ton trito arithmo: "))
 
 D=b**2-4*a*c
 
+if D>0:
+  x1=(-b+math.sqrt(D))/(2*a)
+  x2=(-b-math.sqrt(D))/(2*a)
+  print("H diakrinousa einai: " , D)
+  print (" H proti riza exei apotelesma: " , x1, " kai h deyteri riza exei apotelesma: " , x2)
 
-x1=(-b+math.sqrt(D))/(2*a)
-x2=(-b-math.sqrt(D))/(2*a)
-
-
-
-print("H diakrinousa einai: " , D)
-print (" H proti riza exei apotelesma: " , x1, " kai h deyteri riza exei apotelesma: " , x2)
+else:
+  print("H diakrinousa einai arnitiki. Den iparxoun lisis ")
