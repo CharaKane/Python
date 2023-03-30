158c13ae3218ba0f01ef9fa4e5c2b79425ea76ba
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 17:42:07 +0300

    perimetros trigonou

---
+++ askisi11.py
@@ -0,0 +1,9 @@
+a= float (input ("dose tin pleura 1 tou trigonou : "))
+b= float (input ("dose tin pleura 2 tou trigonou : "))
+c= float (input ("dose tin pleura 3 tou trigonou : "))
+
+per= a+b+c
+
+print("H perimetros tou trigonou einai : " , per)
+
+
