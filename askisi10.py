114096d02a836227733d91c8ba6495ee5a873f5b
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-30 17:26:02 +0300

    diakrinousa

---
+++ askisi10.py
@@ -0,0 +1,16 @@
+import math
+
+a= int (input ("dose ton proto arithmo: "))
+b= int (input ("dose ton deytero arithmo: "))
+c= int (input ("dose ton trito arithmo: "))
+
+D=b**2-4*a*c
+
+
+x1=(-b+math.sqrt(D))/(2*a)
+x2=(-b-math.sqrt(D))/(2*a)
+
+
+
+print("H diakrinousa einai: " , D)
+print (" H proti riza exei apotelesma: " , x1, " kai h deyteri riza exei apotelesma: " , x2)
