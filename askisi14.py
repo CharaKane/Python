e4593ea13361ec8574284bf8f4bc9afc7d71878f
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-04-03 17:09:00 +0300

    imeres evbomadas if

---
+++ askisi14.py
@@ -0,0 +1,17 @@
+a= int(input ("dvse ena arithmo: 1.Monday \n 2.Tuesday \n 3.Wednesday \n 4.Thursday \n 5.Friday \n 6.Saturday \n 7.Sunday "))
+
+if a==1:
+    print("Monday")
+elif a==2:
+    print("Tuesday")
+elif a==3:
+    print("Wednesday")
+elif a==4:
+    print("Thursday")
+elif a==5:
+    print("Friday")  
+elif a==6:
+    print("Saturday")
+else:
+    print("Sunday")
+   
