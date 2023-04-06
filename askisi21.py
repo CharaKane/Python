4ec0934f64a698e6d099972f9f40196733a413e7
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-04-06 17:58:00 +0300

    megaliteros

---
+++ askisi21.py
@@ -0,0 +1,15 @@
+a= float (input ("Dose ton 1 dekadiko arithmo:"))
+b= float (input ("Dose ton 2 dekadiko arithmo:"))
+c= float (input ("Dose ton 3 dekadiko arithmo:"))
+
+m=a
+
+if m<b:
+    m=b
+    print("O megaliteros arithmos einai o:", m)
+elif m<c:
+    m=c
+    print("O megaliteros arithmos einai o:", m)
+else:
+    print("O megaliteros arithmos einai o:", m)
+    
