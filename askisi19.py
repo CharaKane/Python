41d863be4df83e0c8f83f6ba80d811d704d85b15
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-04-06 17:07:52 +0300

    arntikos miden thetikos

---
+++ askisi19.py
@@ -0,0 +1,9 @@
+x= float (input ("Dose enan dekadiko arithmo:"))
+
+
+if x==0.0:
+    print("O arithmos einai mhden")
+elif x>0.0:
+    print("O arithmos einai thetikos")
+else:
+    print("O arithmos einai arnitikos")
