31b26587da753147eab6ea620b8a362c63e96450
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-05-11 19:09:55 +0300

    while

---
+++ askisi24.py
@@ -0,0 +1,15 @@
+x= int (input("dose enan arithmo:"))
+a=0
+i=1
+
+while(x!=0):
+    x= int (input("dose enan arithmo:"))
+    a=a+x
+    i=i+1
+   
+print("To athroisma tous einai:", a)
+mo=a/i
+print("O mesos oros tous einai:", mo)
+print("To plhthos tous einai:", i)
+
+
