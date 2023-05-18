a3198a5161b73194ebfe2fb1a9c152db5eed4f19
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-05-18 18:31:17 +0300

    while

---
+++ askisi25.py
@@ -0,0 +1,12 @@
+s=0
+p=0
+i=1
+while i<=3:
+ a= float(input("Dose tin timi"))
+ b= int(input("Dose posotita"))
+ i=i+1
+ c=a*b
+ s=s+c
+ p=p+b
+print("To sinoliko tous kostos einai:",s)
+print("H sinoliki posotita einai:",p)
